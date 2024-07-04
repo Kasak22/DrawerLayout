f4ed53915dec367a2f0319bb94fc20f55e587937
Kasak Shah <shahkasak7@gmail.com>
2024-07-04 16:10:10 +0530

    this is my new project

---
+++ app/src/main/java/com/example/drawerlayout/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.drawerlayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.maindrawer);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navmenu);
+
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+    }
+}
